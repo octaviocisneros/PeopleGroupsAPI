38e2506f759527352a3b0e3ef771f329cb83c98f
William Criss <wcriss44@gmail.com>
2017-03-17 15:33:25 -0400

    Try new heroku approach

--- src/main/java/com/example/Controllers/PeopleGroupsApiController.java
+++ src/main/java/com/example/Controllers/PeopleGroupsApiController.java
@@ -4,9 +4,15 @@ package com.example.Controllers;
 import com.example.entities.User;
 import com.example.services.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
 import java.util.List;
 
 @RestController
@@ -34,6 +40,15 @@ public class PeopleGroupsApiController {
     @RequestMapping(path = "/user/{id}", method = RequestMethod.GET)
     public User getUser(@PathVariable("id") int id) {
         return users.findOne(id);
+    }
+    @Configuration
+    public class DatabaseConfig {
+        @Bean
+        @Primary
+        @ConfigurationProperties(prefix = "spring.datasource")
+        public DataSource dataSource() {
+            return DataSourceBuilder.create().build();
+        }
     }
 
     @PostConstruct
